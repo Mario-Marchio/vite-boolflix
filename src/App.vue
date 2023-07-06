3f065761547b12055cc6e3dd8779bd08c8fd1cf7
Mario-Marchio <mariomarchio1610@gmail.com>
2023-07-06 17:32:13 +0200

    added series

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,19 @@ export default {
     setTitleFilter(term) {
       this.titleFilter = term;
     },
-    searchMovie() {
+    searchTitle() {
       if (!this.titleFilter) {
         store.movies = [];
+        store.series = [];
         return;
       };
       axios.get(`${api.baseUri}/search/movie?api_key=${api.key}&language=${api.language}&query=${this.titleFilter}`)
         .then(res => {
           store.movies = res.data.results;
+        })
+      axios.get(`${api.baseUri}/search/tv?api_key=${api.key}&language=${api.language}&query=${this.titleFilter}`)
+        .then(res => {
+          store.series = res.data.results;
         })
     }
   }
@@ -31,13 +36,20 @@ export default {
 </script>
 
 <template>
-  <searchbar @term-change="setTitleFilter" @form-submit="searchMovie" />
+  <searchbar @term-change="setTitleFilter" @form-submit="searchTitle" />
   <h1>MOVIES</h1>
   <ul v-for="movie in store.movies">
     <li>{{ movie.title }}</li>
     <li>{{ movie.original_title }}</li>
     <li>{{ movie.original_language }}</li>
     <li>{{ movie.vote_average }}</li>
+  </ul>
+  <h1>SERIES</h1>
+  <ul v-for="serie in store.series">
+    <li>{{ serie.name }}</li>
+    <li>{{ serie.original_name }}</li>
+    <li>{{ serie.original_language }}</li>
+    <li>{{ serie.vote_average }}</li>
   </ul>
 </template>
 
